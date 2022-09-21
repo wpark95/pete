19bda1a08a9c435b4bc5bb2fc545d22a5b7935c6
Will Park <park.wongyun@gmail.com>
2022-09-21 15:58:27 -0700

    Complete step 1 of the game scenario

--- find_pete.py
+++ find_pete.py
@@ -33,6 +33,19 @@ def main():
     def print_game_text(text):
         print_slowly(text, 0.1)
 
+    def prompt_user_choice(l1, l2):
+        print("--- What's your choice? Enter 1 or 2")
+        print("1 - " + l1)
+        print("2 - " + l2)
+        user_choice = 0
+        while user_choice == 0:
+            res = int(input(">>>>> "))
+            if (res == 1):
+                user_choice = 1
+            elif (res == 2):
+                user_choice = 2
+        return user_choice
+
     # print introductory texts
     print_slowly("--- September 21, 2022 | Hershey, PA", 0.2)
     print_time()
@@ -46,27 +59,42 @@ def main():
     curr_time = "06:02 AM"
     curr_location = "Town Square Parking Lot"
 
-    while curr_step < 4:
+    while curr_step < 10:
         print_time()
         print_location()
         print()
 
-        if curr_day == 1:
+        if curr_step == 1:
             print_game_text("--- It's still dark in Hershey. The air is cold and crisp, and, for some reason, heavy.") 
             print_game_text("--- More and more police vehicles are arriving at the scene.")
             print_game_text("You: \"Any updates, detective?\"")
             print_game_text("Z: \"Forensics just came in. Victim is 32 year-old. Male.\"")
             print_game_text("You: \"Name?\"")
-            print_game_text("Z: \"Pete. Guy lives in Hummelstown.\"")
-            print_game_text("You: \"Any leads?\"")
-            curr_location = "Hummelstown, PA"
-            curr_time = "06:48 AM"
+            print_game_text("Z: \"Pete Larson. Guy lives in Hummelstown.\"")
+            print_game_text("You: \"I will go talk to his neighbors.\"")
+            print_game_text("Z: \"I will send the new guy there. Top wants you to meet the victim's parents.\"")
+            user_res = prompt_user_choice("Go to Pete's apartment.", "Meet Pete's parents")
+            if user_res == 1:
+                curr_location = "Hummelstown, PA"
+                curr_time = "06:32 AM"
+            if user_res == 2:
+                curr_location = "Harrisburg, PA"
+                curr_time = "06:46 AM"
+            curr_step += 1
 
-        if curr_step == 2 and curr_location == "Hummelstown, PA":
-            print_game_text("--- The neighborhood is still quite. You are the only person on the road.")
-            print_game_text("--- You walk in to an apartment building, take a deep breath, and knock on a door.")
+        if curr_step == 2:
+            if curr_location == "Hummelstown, PA":
+                print_game_text("--- The neighborhood is still quite. You are the only person on the road.")
+                print_game_text("--- You walk in to an apartment building. You notice a guy sleeping in the hallway.")
 
-        curr_step += 1
+            if curr_location == "Harrisburg, PA":
+                print_game_text("--- The neighborhood is still quite. You are the only person on the road.")
+                print_game_text("--- You park your car in front of the only lit-up house in the neighborhood.")
+                print_game_text("--- You take a deep breath and knock on the door")
 
+            curr_step += 1
+
+        curr_step += 1 # this is for during development (otherwise curr_step will never increment until fully developed)
+        
 if __name__ == "__main__":
     main()
