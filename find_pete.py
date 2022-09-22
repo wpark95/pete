d95c72e4692c7d6c2a819b3aeea27678a5456314
Will Park <park.wongyun@gmail.com>
2022-09-21 17:29:22 -0700

    Modify game scenario texts for step 2

--- find_pete.py
+++ find_pete.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-""" Finding Pete Game | Will Park """
+""" Text-based truth-finding game | Will Park """
 
+# import necessary modules
 import time
 import sys
 
 def main():
 
+    # game state information that decides the scenario
     curr_step = 1
-    curr_time = "05:30 AM"
+    curr_time = "04:22 AM"
     curr_location = "Unknown"
     proof = []
 
-    # slowly prints to the terminal (one letter at a time on the same line)
+    # prints text to the terminal with certain interval per letter
     def print_slowly(text, delay):
         for letter in text:
             sys.stdout.write(letter)
@@ -19,17 +21,21 @@ def main():
             time.sleep(delay)
         print()
 
+    # prints the current game scenario time using the print_slowly function
     def print_time():
-        time_script = "----- Time: " + curr_time
+        time_script = "----- Time: " + curr_time + " -----"
         print_slowly(time_script, 0.1)
 
+    # prints the current game scenario location using the print_slowly function
     def print_location():
-        location_script = "----- Location: " + curr_location
+        location_script = "----- Location: " + curr_location + " -----"
         print_slowly(location_script, 0.1)
 
+    # prints the current game texts for each scene at a relatively fast pace 
     def print_game_text(text):
         print_slowly(text, 0.05)
 
+    # prompts the user to enter their next move which will decide the next scenario
     def prompt_user_choice(l1, l2):
         print()
         print("--- What's your choice? Enter 1 or 2")
@@ -44,19 +50,20 @@ def main():
                 user_choice = 2
         return user_choice
 
-    # print introductory texts
-    print_slowly("----- September 21, 2022 | Hershey, PA", 0.2)
+    # prints introductory texts at the beginning of the game
+    print_slowly("----- September 21, 2022 | Hershey, PA -----", 0.175)
     print_time()
     print_location()
     print()
-    print_game_text("Bzzt... Bzzt...")
-    print_game_text("--- You wake up. Your eyes are still blurry but you manage to find your cell phone.")
+    print_game_text("Bzzt... Bzzt... Bzzt...")
+    print_game_text("--- You wake up. Your eyes are still blurry but you manage to find your cell phone underneath your pillow.")
     print()
     print_game_text("You: \"Hello?\"")
     print_game_text("Z: \"Hey, it's me, Z. Listen, I need you to come to the town square parking lot. Now.\"")
-    curr_time = "06:02 AM"
+    curr_time = "05:02 AM"
     curr_location = "Town Square Parking Lot"
 
+    # 
     while curr_step < 4:
         print()
         print()
@@ -65,36 +72,39 @@ def main():
         print()
 
         if curr_step == 1:
-            print_game_text("--- It's still dark in Hershey. The air is cold and crisp, and, for some reason, heavy.") 
-            print_game_text("--- More and more police vehicles are arriving at the scene.")
+            print_game_text("--- It's still dark in Hershey. The air is cold and crisp. And for some reason, feels heavy.") 
+            print_game_text("--- You walk past police vehicles surrounding the parking lot.")
+            print_game_text("--- You see Z standing next to a car.")
             print()
-            print_game_text("You: \"What do we have here?\"")
-            print_game_text("Z: \"Someone found . Victim is 32 year-old. Male.\"")
-            print_game_text("You: \"Name?\"")
-            print_game_text("Z: \"Pete Larson. Guy lives in Hummelstown.\"")
-            print_game_text("You: \"I'll go talk to his neighbors.\"")
-            print_game_text("Z: \"Not right now. Top wants you to meet the victim's parents.\"")
+            print_game_text("You: \"What do we have here, Z?\"")
+            print_game_text("Z: \"Someone called 911 an hour ago. They heard a gunshot in this parking lot.\"")
+            print_game_text("You: \"And?\"")
+            print_game_text("Z: \"When Sergeant Jones got here, he found the victim in this car.\"")
+            print_game_text("You: \"Do we know the victim?\"")
+            print_game_text("Z: \"Pete. Pete Larson. Guy lives in Hummelstown.\"")
+            print_game_text("You: \"I'll go to Hummelstown and see if I can find something there.\"")
+            print_game_text("Z: \"Not right now. Chief wants you to meet the victim's parents.\"")
             user_res = prompt_user_choice("Go to Pete's apartment", "Meet Pete's parents")
 
             # Based on your's choice, update scenario info
             if user_res == 1:
-                print_game_text("You: \"Z, I live near his neighborhood. I will see if I can find something there.\"")
+                print_game_text("You: \"Z, my guts are telling me something's there. I'll call chief on my way there.\"")
                 curr_location = "Hummelstown, PA"
-                curr_time = "06:32 AM"
+                curr_time = "05:29 AM"
             if user_res == 2:
-                print_game_text("You: \"Fine. I'll meet his parents.\"")
+                print_game_text("You: \"Chief said that? Fine. I'll meet his parents and see if I can find something there.\"")
                 curr_location = "Harrisburg, PA"
-                curr_time = "06:46 AM"
+                curr_time = "05:41 AM"
 
         if curr_step == 2:
             if curr_location == "Hummelstown, PA":
                 print_game_text("--- The neighborhood is still quite. You are the only person on the road.")
-                print_game_text("--- You walk in to an apartment building. You notice a guy sleeping in the hallway.")
+                print_game_text("--- You park your car and walk in to Pete's apartment building. The hallway is dead silent.")
 
             if curr_location == "Harrisburg, PA":
                 print_game_text("--- The neighborhood is still quite. You are the only person on the road.")
                 print_game_text("--- You park your car in front of the only lit-up house in the neighborhood.")
-                print_game_text("--- You take a deep breath and knock on the door")
+                print_game_text("--- You take a deep breath and knock on the door.")
 
         # increment the game progress (curr_step)
         curr_step += 1
